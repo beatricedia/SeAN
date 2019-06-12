3019c2ccbeb91ae87dc13b2c2bdf5a2cae234d79
beatricedia <beatricedia1@gmail.com>
2019-06-12 15:23:46 +0200

    Deleted checkboxes functions from register.js

--- MVC/scripts/register.js
+++ MVC/scripts/register.js
@@ -1,46 +1,8 @@
-var expanded1 = false;
-
 function validateEmail(email) {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
 
-function showCheckboxes1() {
-    var checkboxes1 = document.getElementById("checkboxes1");
-    if (!expanded1) {
-        checkboxes1.style.display = "block";
-        expanded1 = true;
-    } else {
-        checkboxes1.style.display = "none";
-        expanded1 = false;
-    }
-}
-
-var expanded2 = false;
-
-function showCheckboxes2() {
-    var checkboxes2= document.getElementById("checkboxes2");
-    if (!expanded2) {
-        checkboxes2.style.display = "none";
-        expanded2 = true;
-    } else {
-        checkboxes2.style.display = "block";
-        expanded2 = false;
-    }
-}
-
-var expanded3 = false;
-
-function showCheckboxes3() {
-    var checkboxes3= document.getElementById("checkboxes3");
-    if (!expanded3) {
-        checkboxes3.style.display = "none";
-        expanded3 = true;
-    } else {
-        checkboxes3.style.display = "block";
-        expanded3 = false;
-    }
-}
 
 function postRegister(json,callback)
 {
